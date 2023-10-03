9f404b2861e208e2bb2942a7676e623e107861a8
Albert Florido <Albertf313@gmail.com>
2023-10-03 13:53:41 -0400

    Initial Commit

---
+++ onZenTEST/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  OnZenRemake
+//
+//  Created by Albert Florido  on 10/2/23.
+//
+import SwiftUI
+
+struct ContentView: View {
+    @State private var prompts: [String] = ["who","when","what","where","why"]
+    
+    
+    @State var isToggle: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color( isToggle ? .purple : .pink)
+                .ignoresSafeArea()
+            
+            VStack {
+                infoButton()
+                Image("test")
+                    .padding()
+                
+                HStack {
+                    Text("Release")
+                        .font(Font.custom("SFProRounded-Medium", size: 20))
+                        .padding(.leading)
+                    
+                    Spacer().frame (width: 20)
+                    
+                    
+                    Toggle("",isOn: $isToggle)
+                        .frame(width: 80)
+                        
+                    
+                    
+                    Spacer().frame(width: 60 )
+                    
+                    Text("Reflect")
+                        .font(Font.custom("SFProRounded-Medium", size: 20))
+                        .padding(.trailing)
+                    
+                }
+                VStack(spacing: 20) {
+                    mainButton(buttonPrompt: prompts[0])
+                    mainButton(buttonPrompt: prompts[2])
+                    mainButton(buttonPrompt: prompts[1])
+                    mainButton(buttonPrompt: prompts[3])
+                    mainButton(buttonPrompt: prompts[4])
+                }
+                Spacer()
+                Image("zenCircle")
+                    .resizable()
+                    .frame(width: 75, height: 75)
+                
+            }
+            
+            
+            
+        }
+        
+    }
+ 
+}
+
+#Preview {
+    ContentView()
+}
+
+struct mainButton: View {
+    var buttonPrompt: String
+    var body: some View {
+       
+        HStack{
+            Text(buttonPrompt)
+                .font(Font.custom("SFProRounded-Medium", size: 20))
+                .frame(width: 350, height: 60)
+                .background(.reflectButton)
+                
+        }
+    }
+}
+
+struct infoButton: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            Image(systemName: "questionmark.circle")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .padding(.trailing)
+            
+        }
+    }
+}
---
+++ onZenTEST/onZenTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  onZenTESTApp.swift
+//  onZenTEST
+//
+//  Created by Albert Florido  on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct onZenTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
